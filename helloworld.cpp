15c5cc2c203e1a7adf9c6c804b174ee23a9a0c83
sharansrinivaas <98168824+sharansrinivaas@users.noreply.github.com>
2022-11-15 11:12:28 +0000

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include<iostream> //Header Files
+using namespace std;    //helps to separate these identifiers from similar identifiers in other namespaces or the global namespace.
+
+int main()          //main fn of the code
+{
+    cout<<"Hello World";   //output
+
+}
