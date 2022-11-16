2bd44d185b0eaf20693d527adf7340ffa8edfeab
sharansrinivaas <98168824+sharansrinivaas@users.noreply.github.com>
2022-11-16 18:34:08 +0000

    Basics of C++ Input/Output

---
+++ lesson2basicinput.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //Header Files
+using namespace std;
+
+int main()          //main function of the code
+{   
+    int x;          //declare a variable for the computer to read input from, where x is variable name and int(short for integer) is the data type
+    cout<<"Enter Input: ";      
+    cin>>x;         //input value for x, where cin means "Character in"
+    cout<<"\nOutput is: "<<x; //Display output, \n pushes the output to a new line. Alternatively, this can also be written as cout<<endl<<"Output is: "<<x;
+}
